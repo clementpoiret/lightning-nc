69b6e341475701665f48e446a05eb67645d0207d
Clément POIRET <poiret.clement@outlook.fr>
2023-11-15 14:40:56 +0100

    fix: incompatibility between the two callbacks

--- lightning_nc/callbacks.py
+++ lightning_nc/callbacks.py
@@ -11,10 +11,10 @@ from neural_compressor.compression.pruner.utils import (get_sparsity_ratio,
                                                         parse_to_prune,
                                                         process_config)
 from neural_compressor.config import Options, options
-from neural_compressor.model.model import Model
+from neural_compressor.model.model import BaseModel, Model
 from neural_compressor.training import WeightPruningConfig
-from torch.utils.data import DataLoader
 from torch.optim import Optimizer
+from torch.utils.data import DataLoader
 
 logger = logging.getLogger(__name__)
 
@@ -40,7 +40,6 @@ class QATCallback(Callback):
             "format": quant_format,
             "approach": config.approach,
         }
-        print("QATCallback: ", framework_specific_info)
 
         # WARNING: May change in a future version of Intel(R) Neural Compressor
         framework_specific_info["qat_optype_wise"] = config.op_type_dict
@@ -48,18 +47,22 @@ class QATCallback(Callback):
 
         self.adaptor = PyTorchAdaptor(framework_specific_info)
 
-    def on_train_start(self, trainer: Trainer,
-                       pl_module: LightningModule) -> None:
-        self.adaptor.model = pl_module.model = Model(pl_module.model,
-                                                     conf=self.config)
-        self.adaptor._pre_hook_for_qat(dataloader=self.dataloader)
+    def setup(self, trainer: Trainer, pl_module: LightningModule,
+              stage: str) -> None:
+        if stage == "fit":
+            if isinstance(pl_module.model, BaseModel):
+                self.adaptor.model = pl_module.model.model = Model(
+                    pl_module.model.model, conf=self.config)
+            else:
+                self.adaptor.model = pl_module.model = Model(pl_module.model,
+                                                             conf=self.config)
 
-    def on_train_end(self, trainer: Trainer,
-                     pl_module: LightningModule) -> None:
-        self.adaptor._post_hook_for_qat()
+            self.adaptor._pre_hook_for_qat(dataloader=self.dataloader)
 
-
-# TODO: before and after eval
+    def teardown(self, trainer: Trainer, pl_module: LightningModule,
+                 stage: str) -> None:
+        if stage == "fit":
+            self.adaptor._post_hook_for_qat()
 
 
 class WeightPruningCallback(Callback):
@@ -70,16 +73,15 @@ class WeightPruningCallback(Callback):
         super().__init__()
         self.config = config
         self.dataloader = dataloader
+
         self.pruners_info = process_config(config)
-        self.model = None
         self.pruners = []
-        self.local_step = 0
 
     def _generate_pruners(self, model: Model) -> None:
         for info in self.pruners_info:
             if "mha" in info["pattern"]:
                 # head pruning
-                pa_obj = SelfMHASearcher(self.model.model)
+                pa_obj = SelfMHASearcher(model.model)
                 modules, _ = pa_obj.search(split_qkv_ffn=False)
                 modules = pa_obj.obtain_mha_module(modules)
                 modules = pa_obj.from_layer_name_to_object(modules)
@@ -89,7 +91,7 @@ class WeightPruningCallback(Callback):
                 self.pruners.append(get_pruner(info, modules))
             else:
                 # original pruning types, e.g NxM or N:M
-                modules = parse_to_prune(info, self.model.model)
+                modules = parse_to_prune(info, model.model)
                 if modules == {}:
                     logger.warning(
                         "one pruner hooks no layers, please have a check")
@@ -97,16 +99,22 @@ class WeightPruningCallback(Callback):
                 self.pruners.append(get_pruner(info, modules))
                 info["modules"] = [key for key in modules.keys()]
                 info["len_of_modules"] = len(info["modules"])
+
                 logger.info(info)
+
+    def setup(self, trainer: Trainer, pl_module: LightningModule, stage: str):
+        if stage == "fit":
+            print("setup weight pruning callback")
+            if isinstance(pl_module.model, BaseModel):
+                pl_module.model.model = Model(pl_module.model.model,
+                                              conf=self.config)
+            else:
+                pl_module.model = Model(pl_module.model, conf=self.config)
 
     def on_train_start(self, trainer: Trainer,
                        pl_module: LightningModule) -> None:
-        current_epoch = trainer.current_epoch
 
-        if current_epoch == 0:
-            self.model = pl_module.model = Model(pl_module.model,
-                                                 conf=self.config)
-            self._generate_pruners(self.model)
+        self._generate_pruners(pl_module.model)
 
         for pruner in self.pruners:
             pruner.on_train_begin(dataloader=self.dataloader)
@@ -120,15 +128,11 @@ class WeightPruningCallback(Callback):
 
     def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
         for pruner in self.pruners:
-            pruner.on_step_begin(self.local_step)
-
-        self.local_step += 1
+            pruner.on_step_begin(batch_idx)
 
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
         for pruner in self.pruners:
             pruner.on_step_end()
-
-        self.local_step = 0
 
     def on_train_epoch_end(self, trainer: Trainer,
                            pl_module: LightningModule) -> None:
@@ -152,4 +156,14 @@ class WeightPruningCallback(Callback):
         for pruner in self.pruners:
             pruner.on_train_end()
 
-        get_sparsity_ratio(self.pruners, self.model)
+        get_sparsity_ratio(self.pruners, pl_module.model)
+
+    def on_validation_epoch_start(self, trainer: Trainer,
+                                  pl_module: LightningModule) -> None:
+        for pruner in self.pruners:
+            pruner.on_before_eval()
+
+    def on_validation_epoch_end(self, trainer: Trainer,
+                                pl_module: LightningModule) -> None:
+        for pruner in self.pruners:
+            pruner.on_after_eval()
