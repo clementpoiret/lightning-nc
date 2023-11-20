eb38b40404d2865c3e5eab6e70cfaeb88656142b
Clément POIRET <poiret.clement@outlook.fr>
2023-11-20 19:27:55 +0100

    feat: finetuning callback

--- lightning_nc/__init__.py
+++ lightning_nc/__init__.py
@@ -1,3 +1,5 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
-from .callbacks import QATCallback, WeightPruningCallback
+from .finetuning import FinetuningCallback
+from .pruning import WeightPruningCallback
+from .quantization import QATCallback
--- lightning_nc/pruning.py
+++ lightning_nc/pruning.py
@@ -8,105 +8,18 @@ import logging
 from typing import Any, Optional
 
 from lightning.pytorch import Callback, LightningModule, Trainer
-from neural_compressor import QuantizationAwareTrainingConfig
-from neural_compressor.adaptor.pytorch import PyTorchAdaptor
 from neural_compressor.compression.pruner.model_slim.pattern_analyzer import \
     SelfMHASearcher
 from neural_compressor.compression.pruner.pruners import get_pruner
 from neural_compressor.compression.pruner.utils import (get_sparsity_ratio,
                                                         parse_to_prune,
                                                         process_config)
-from neural_compressor.config import Options, options
 from neural_compressor.model.model import BaseModel, Model
 from neural_compressor.training import WeightPruningConfig
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
 logger = logging.getLogger(__name__)
-
-
-class QATCallback(Callback):
-    """Quantization aware training callback for PyTorch Lightning
-
-    Attributes
-    ----------
-    config: QuantizationAwareTrainingConfig
-        Configuration for quantization aware training.
-    adaptor: PyTorchAdaptor
-        Adaptor of PyTorch framework, all PyTorch API is in this class.
-
-    Methods
-    -------
-    setup(trainer, pl_module, stage)
-        Setup the callback.
-    teardown(trainer, pl_module, stage)
-        Teardown the callback.
-    """
-
-    def __init__(self,
-                 config: QuantizationAwareTrainingConfig,
-                 backend: str = "default",
-                 quant_format: str = "default",
-                 options: Options = options,
-                 dataloader: Optional[DataLoader] = None) -> None:
-        """
-        Parameters
-        ----------
-        config : QuantizationAwareTrainingConfig
-            Configuration for quantization aware training.
-        backend : str, optional
-            Backend of quantization aware training, by default "default"
-        quant_format : str, optional
-            Format of quantization in ONNXRuntime, by default "default"
-        options : Options, optional
-            Options for quantization aware training
-        dataloader : Optional[DataLoader], optional
-            Dataloader for accuracy calibration, by default None
-        """
-        super().__init__()
-        self.config = config
-        self.dataloader = dataloader
-
-        # Taken from
-        # https://github.com/intel/neural-compressor/blob/d81269d2b261d39967605e17a89b5688ebaedbd1/neural_compressor/training.py#L130
-        framework_specific_info = {
-            "device": config.device,
-            "random_seed": options.random_seed,
-            "workspace_path": options.workspace,
-            "q_dataloader": None,
-            "backend": backend,
-            "format": quant_format,
-            "approach": config.approach,
-        }
-
-        # WARNING: May change in a future version of Intel(R) Neural Compressor
-        framework_specific_info["qat_optype_wise"] = config.op_type_dict
-        framework_specific_info["qat_op_wise"] = config.op_name_dict
-
-        self.adaptor = PyTorchAdaptor(framework_specific_info)
-
-    def setup(self, trainer: Trainer, pl_module: LightningModule,
-              stage: str) -> None:
-        """Setup the callback by converting the model.
-        Original hook defined here: https://github.com/intel/neural-compressor/blob/d81269d2b261d39967605e17a89b5688ebaedbd1/neural_compressor/compression/callbacks.py#L194
-        """
-        if stage == "fit":
-            if isinstance(pl_module.model, BaseModel):
-                self.adaptor.model = pl_module.model.model = Model(
-                    pl_module.model.model, conf=self.config)
-            else:
-                self.adaptor.model = pl_module.model = Model(pl_module.model,
-                                                             conf=self.config)
-
-            self.adaptor._pre_hook_for_qat(dataloader=self.dataloader)
-
-    def teardown(self, trainer: Trainer, pl_module: LightningModule,
-                 stage: str) -> None:
-        """Teardown the callback.
-        Original hook defined here: https://github.com/intel/neural-compressor/blob/d81269d2b261d39967605e17a89b5688ebaedbd1/neural_compressor/compression/callbacks.py#L195
-        """
-        if stage == "fit":
-            self.adaptor._post_hook_for_qat()
 
 
 class WeightPruningCallback(Callback):
